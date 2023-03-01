e25a54e9b56d925d650022be7ca2320822788aca
ksuares1 <kimberlyjsuares@gmail.com>
2023-03-01 16:28:43 -0500

    updated variables and conditional statements on js file

--- assets/app.js
+++ assets/app.js
@@ -35,16 +35,11 @@ let celsiusTemperature = Math.round(temperature);
 let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
 
 function atlas(){
-   if (city === city)  {
+   if (weather[city] !== undefined)  {
         alert(`It is currently ${celsiusTemperature} °C ${fahrenheitTemperature} °F in ${city} with a humidity of ${humidity}% `
         );
     }
-    else if(weather[city] !== undefined){
-       alert(
-         `It is currently ${celsiusTemperature} °C ${fahrenheitTemperature} °F in ${city} with a humidity of ${humidity}% `
-       );
-    }
-
+  
      else {
       alert(
         "Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+sydney");

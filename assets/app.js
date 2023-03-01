7824b2c21e58a939baff55ba16f3e699e0cb3fae
ksuares1 <kimberlyjsuares@gmail.com>
2023-03-01 15:59:05 -0500

    updated alert

--- assets/app.js
+++ assets/app.js
@@ -27,16 +27,16 @@ let weather = {
 // write your code here
  
 let city = prompt("Enter your city:");
-city=city.toLowerCase();
-city=city.trim();
-let humidity = weather[city].humidity;
-let temperature = weather[city].temp;
-let celsiusTemperature = Math.round(temperature);
-let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
+// city=city.toLowerCase();
+// city=city.trim();
+// let humidity = weather[city].humidity;
+// let temperature = weather[city].temp;
+// let celsiusTemperature = Math.round(temperature);
+// let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+// || city === "Tokyo" || city === "Lisbon" || city === "San Francisco" || city === "Oslo"
 function atlas(){
-   if (city === "Paris" || city === "Tokyo" || city === "Lisbon" || city === "San Francisco" || city === "Oslo")  {
-        alert(`It is currently ${celsiusTemperature}°C ${fahrenheitTemperature}°F in ${city} with a humidity of ${humidity}% `);
+   if (city === "Paris" )  {
+        alert(`It is currently 19°C (66°F) in ${city} with a humidity of 80% `);
 
     }
      else {
